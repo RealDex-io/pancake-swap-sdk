c56f84690c6a71030287bd0010a410401d3f5a0b
Dũng Nguyễn <realdex.io@gmail.com>
2022-05-12 11:05:19 +0700

    Revert PAIR_ADDRESS_CACHE

--- src/entities/pair.ts
+++ src/entities/pair.ts
@@ -21,7 +21,7 @@ import { sqrt, parseBigintIsh } from '../utils'
 import { InsufficientReservesError, InsufficientInputAmountError } from '../errors'
 import { Token } from './token'
 
-let PAIR_ADDRESS_CACHE: { [token0Address: string]: { [token1Address: string]: string } } = {}
+let PAIR_ADDRESS_CACHE: { [token0Address: string]: { [token1Address: string]: { [aFactor: string] : string } } } = {}
 
 export class Pair {
   public readonly liquidityToken: Token
@@ -31,21 +31,24 @@ export class Pair {
   public static getAddress(tokenA: Token, tokenB: Token, aFactor: BigintIsh): string {
     const tokens = tokenA.sortsBefore(tokenB) ? [tokenA, tokenB] : [tokenB, tokenA] // does safety checks
 
-    if (PAIR_ADDRESS_CACHE?.[tokens[0].address]?.[tokens[1].address] === undefined) {
+    if (PAIR_ADDRESS_CACHE?.[tokens[0].address]?.[tokens[1].address]?.[aFactor.toString()] === undefined) {
       PAIR_ADDRESS_CACHE = {
         ...PAIR_ADDRESS_CACHE,
         [tokens[0].address]: {
           ...PAIR_ADDRESS_CACHE?.[tokens[0].address],
-          [tokens[1].address]: getCreate2Address(
-            FACTORY_ADDRESS,
-            keccak256(['bytes'], [pack(['address', 'address', 'uint256'], [tokens[0].address, tokens[1].address, aFactor?.toString()])]),
-            INIT_CODE_HASH
-          ),
+          [tokens[1].address]: {
+            ...PAIR_ADDRESS_CACHE?.[tokens[0].address]?.[tokens[1].address],
+            [aFactor.toString()]: getCreate2Address(
+                FACTORY_ADDRESS,
+                keccak256(['bytes'], [pack(['address', 'address', 'uint256'], [tokens[0].address, tokens[1].address, aFactor.toString()])]),
+                INIT_CODE_HASH
+              ),
+          }
         },
       }
     }
 
-    return PAIR_ADDRESS_CACHE[tokens[0].address][tokens[1].address]
+    return PAIR_ADDRESS_CACHE[tokens[0].address][tokens[1].address][aFactor.toString()]
   }
 
   public constructor(tokenAmountA: TokenAmount, tokenAmountB: TokenAmount, aFactor: BigintIsh) {
